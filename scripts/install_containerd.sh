267cc755b13b90f55a38ebf88d72ea979a5de67a
pdaambrosio <pda.ambrosio@gmail.com>
2022-10-17 11:54:15 -0300

    feat: create script for install packages

---
+++ scripts/install_containerd.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+sudo cat > /etc/modules-load.d/containerd.conf <<EOF
+overlay
+br_netfilter
+EOF
+
+sudo modprobe overlay
+sudo modprobe br_netfilter
+
+sudo cat > /etc/sysctl.d/99-kubernetes-cri.conf <<EOF
+net.bridge.bridge-nf-call-iptables  = 1
+net.ipv4.ip_forward                 = 1
+net.bridge.bridge-nf-call-ip6tables = 1
+EOF
+
+sudo sysctl --system
+
+sudo dnf install -y  yum-utils device-mapper-persistent-data lvm2
+sudo dnf config-manager --add-repo=https://download.docker.com/linux/centos/docker-ce.repo
+sudo dnf update -y && dnf install -y containerd.io
+sudo mkdir -p /etc/containerd
+sudo containerd config default > /etc/containerd/config.toml
+sudo systemctl restart containerd
+sudo systemctl enable containerd
+
+sudo cat <<EOF > /etc/yum.repos.d/kubernetes.repo
+[kubernetes]
+name=Kubernetes
+baseurl=https://packages.cloud.google.com/yum/repos/kubernetes-el7-x86_64
+enabled=1
+gpgcheck=1
+repo_gpgcheck=1
+gpgkey=https://packages.cloud.google.com/yum/doc/yum-key.gpg https://packages.cloud.google.com/yum/doc/rpm-package-key.gpg
+EOF
+
+sudo dnf install -y kubeadm kubelet kubectl
+sudo systemctl enable kubelet
+sudo echo 'KUBELET_EXTRA_ARGS="--fail-swap-on=false"' > /etc/sysconfig/kubelet
+sudo systemctl start kubelet
