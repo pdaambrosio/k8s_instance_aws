a17788a862f9744ebb98960be0e0b004989bd031
pdaambrosio <pda.ambrosio@gmail.com>
2022-10-22 10:26:44 -0300

    fix: indentation

--- output.tf
+++ output.tf
@@ -1,26 +1,26 @@
 output "vpc_default_id" {
-  value = data.aws_vpc.default_vpc.id
+  value       = data.aws_vpc.default_vpc.id
   description = "Default VPC ID"
 }
 
 output "subnet_default_id" {
-  value = data.aws_subnet.default_subnet.id
+  value       = data.aws_subnet.default_subnet.id
   description = "Default Subnet ID"
 }
 
 output "security_group_default_id" {
-  value = data.aws_security_group.default_sg.id
+  value       = data.aws_security_group.default_sg.id
   description = "Default Security Group ID"
 }
 
 output "private_key_control_plane" {
-  value     = module.ec2_kubernetes_control_plane_redhat.private_key
-  sensitive = true
+  value       = module.ec2_kubernetes_control_plane_redhat.private_key
+  sensitive   = true
   description = "Private Key"
 }
 
 output "private_key_worker" {
-  value     = module.ec2_kubernetes_worker_redhat.private_key
-  sensitive = true
+  value       = module.ec2_kubernetes_worker_redhat.private_key
+  sensitive   = true
   description = "Private Key"
 }
